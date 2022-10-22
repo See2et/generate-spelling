e786b2446261adccb073c6c1741cfbf35817daa4
see2et <>
2022-10-22 23:15:34 +0900

    delete a console.log for debug

--- src/functions/spell.ts
+++ src/functions/spell.ts
@@ -2,7 +2,6 @@ import { carryUp } from "./carryUp";
 
 export function spell(value: number, syllable: string[][]) {
   const radixes = syllable.map((value) => value.length);
-  console.log("radixes:", radixes);
   const digits = carryUp(value, radixes);
   const result = digits
     .map((value, index) => syllable[index][value])
