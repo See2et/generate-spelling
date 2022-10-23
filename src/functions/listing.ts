37024e0f588dff2dea986b661d2db5b0855006c3
see2et <>
2022-10-23 22:11:22 +0900

    to get an abs value

--- src/functions/listing.ts
+++ src/functions/listing.ts
@@ -1,18 +1,18 @@
 import { carryUp } from "./carryUp";
 import { numArrayToString } from "./numArrayToString";
 
-export function listing(value: number, syllable: string[][]) {
-  const radixes = syllable.map((value) => value.length);
+export function listing(from: number, to: number, syllable: string[][]) {
+  const radixes = syllable.map((v) => v.length);
   const maxRadix = radixes.reduce((a, b) => Math.max(a, b));
   const indexOfMaxRadix = radixes.indexOf(maxRadix);
   const removedMaxRadix = radixes.slice(indexOfMaxRadix + 1);
 
-  const spellingNum = [...new Array(value)].map((v, i) => {
-    const carriedUp = carryUp(i, removedMaxRadix);
-    const spliced = carriedUp.splice(indexOfMaxRadix, 0, i % maxRadix);
+  const spellingNum = [...new Array(Math.abs(to - from))].map((_, i) => {
+    const carriedUp = carryUp(i + from, removedMaxRadix);
+    const spliced = carriedUp.splice(indexOfMaxRadix, 0, (i + from) % maxRadix);
     return carriedUp;
   });
-  const spelling = spellingNum.map((v, i) => {
+  const spelling = spellingNum.map((v) => {
     const result = numArrayToString(v, syllable);
     return result;
   });
