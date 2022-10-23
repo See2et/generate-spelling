a14b8fc8b4713b0ee697c0dba2da94e12d0569f9
see2et <>
2022-10-23 21:56:12 +0900

    made it smarter

--- src/functions/spell.ts
+++ src/functions/spell.ts
@@ -1,10 +1,9 @@
 import { carryUp } from "./carryUp";
+import { numArrayToString } from "./numArrayToString";
 
 export function spell(value: number, syllable: string[][]) {
   const radixes = syllable.map((value) => value.length);
   const digits = carryUp(value, radixes);
-  const result = digits
-    .map((value, index) => syllable[index][value])
-    .reduce((a, b) => a.concat(b));
+  const result = numArrayToString(digits, syllable);
   return result;
 }
