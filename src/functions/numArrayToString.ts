7a95ea4f906f09afb60918da40a2750613128fb4
see2et <>
2022-10-23 22:01:25 +0900

    made it smarter

--- src/functions/numArrayToString.ts
+++ src/functions/numArrayToString.ts
@@ -1,3 +1,3 @@
 export function numArrayToString(array: number[], syllable: string[][]) {
-  return array.map((v, i) => syllable[i][v]).reduce((a, b) => a.concat(b));
+  return array.map((v, i) => syllable[i][v]).join("");
 }
