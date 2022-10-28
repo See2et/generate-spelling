65a4562de2210288d20ce4042ab6af24fb028f9d
see2et <>
2022-10-28 23:29:12 +0900

    made makeZpDicJson.ts

--- src/functions/makeZpdicJson.ts
+++ src/functions/makeZpdicJson.ts
@@ -1,12 +1,10 @@
-export function makeZpdicJson(words: string[], existing?: JSON) {
-  return words
-    .map((value, index) => {
-      return {
-        entry: {
-          id: index,
-          form: value,
-        },
-      };
-    })
-    .concat(existing);
+export function makeZpdicJson(words: string[]) {
+  return words.map((value, index) => {
+    return {
+      entry: {
+        id: index,
+        form: value,
+      },
+    };
+  });
 }
