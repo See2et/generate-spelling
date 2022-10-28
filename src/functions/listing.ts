9608b89043a67a1ba2e046cdbb1d3eb920d7bee2
see2et <>
2022-10-28 21:35:19 +0900

    delete comments

--- src/functions/listing.ts
+++ src/functions/listing.ts
@@ -18,38 +18,3 @@ export function listing(from: number, to: number, syllable: string[][]) {
   });
   return spelling;
 }
-
-/*export function listing(value: number, syllable: string[][]) {
-  const radixes = syllable.map((value) => value.length);
-  const maxRadix = radixes.reduce((a, b) => Math.max(a, b));
-  const maxIndex = radixes.indexOf(maxRadix);
-  console.log("radixes", radixes);
-  console.log("maxRadiex:", maxRadix);
-  console.log("index:", maxIndex);
-
-  const removedMaxRadix = radixes
-    .slice(0, maxIndex - 1)
-    .concat(radixes.slice(maxIndex));
-  console.log("removedMaxRadix:", removedMaxRadix);
-
-  const arrayOfDigits = [...Array(value)].map((value, index) => {
-    const checksome = index % maxRadix;
-    const digits = carryUp(index, removedMaxRadix);
-    const result = digits.splice(maxIndex, 0, checksome);
-    console.log("index:", index);
-    console.log("checksome:", checksome);
-    console.log("digits:", digits);
-    console.log("result:", result);
-    return result;
-  });
-  console.log("arrayOfDigits:", arrayOfDigits);
-
-  const spelling = arrayOfDigits.map((digits) => {
-    const result = digits
-      .map((value, index) => syllable[index][value])
-      .reduce((a, b) => a.concat(b));
-  });
-  console.log("spelling:", spelling);
-
-  return spelling;
-}*/
