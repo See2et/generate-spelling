e05183d917227a978b6bf267e0ed8dca91853db0
see2et <>
2022-10-28 22:17:53 +0900

    made it non-destructives

--- src/functions/carryUp.ts
+++ src/functions/carryUp.ts
@@ -2,6 +2,7 @@ export function carryUp(value: number, radixes: number[]) {
   if (value >= radixes.reduce((a, b) => a * b))
     throw new TypeError("this value is too large");
   const digits = radixes
+    .slice(0)
     .reverse()
     .map((radix) => {
       const digit = value % radix;
